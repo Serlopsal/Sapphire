ac3e539f9a4110004325b06fd456876b541b410c
Sergio Lopez <58184356+SLopez-GIEG@users.noreply.github.com>
2021-01-19 21:32:19 -0600

    Add JWT

---
+++ API/SapphireApi/Data/Identity/SharedModels/LoginModel.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+
+namespace SapphireApi.Data.Identity.SharedModels{
+  public class LoginModel{
+    public string user { get; set; }
+    public string password { get; set; }
+  }
+}
