5cb983d1bdfcdcae53fa44740abbb4b4082f61ae
Sergio Lopez <58184356+SLopez-GIEG@users.noreply.github.com>
2021-02-11 16:49:34 -0600

    Update docker-image-builder.debug.ps1

--- API/docker-image-builder.debug.ps1
+++ API/docker-image-builder.debug.ps1
@@ -1,3 +1,3 @@
- docker build -t sapphire-api:debug -f dockerfile.debug .
  dotnet publish .\SapphireApi\ -o publish
+ docker build -t sapphire-api:debug -f dockerfile.debug .
  docker-compose -f docker-compose.debug.yml up -d --force-recreate
