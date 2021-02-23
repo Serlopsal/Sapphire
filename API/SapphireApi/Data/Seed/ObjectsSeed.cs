23a36a623ea20c69cff4ec22bfcb3cd087573b11
Sergio Lopez <58184356+SLopez-GIEG@users.noreply.github.com>
2021-02-23 14:38:03 -0600

    Create ObjectsSeed.cs

---
+++ API/SapphireApi/Data/Seed/ObjectsSeed.cs
@@ -0,0 +1,29 @@
+using System;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using SapphireApi.Data.Adminsitration.Setup.Objects;
+using SapphireApi.Data.Identity;
+
+namespace SapphireApi.Data {
+  internal class ObjectSeed {
+    public ObjectSeed(ModelBuilder builder) {
+      builder
+        .Entity<ObjectModel>()
+        .HasData(
+          new ObjectModel(){
+            code = "1",
+            description = "Sapphire Objects",
+            docPrefix = null,
+            defaultSerieId = null
+          }
+          ,
+          new ObjectModel(){
+            code = "1.1",
+            description = "Series",
+            docPrefix = null,
+            defaultSerieId = null
+          }
+        );  
+    }
+  }
+}
