e7d0fa8e0a3c591724cf1a6b79e97bf189e43308
Sergio Jose López Salgado <Serlopsal_199@yahoo.es>
2021-03-19 22:29:09 -0600

    Update ObjectsSeed.cs

--- API/SapphireApi/Data/Seed/ObjectsSeed.cs
+++ API/SapphireApi/Data/Seed/ObjectsSeed.cs
@@ -7,23 +7,23 @@ using SapphireApi.Data.Identity;
 namespace SapphireApi.Data {
   internal class ObjectSeed {
     public ObjectSeed(ModelBuilder builder) {
-      // builder
-      //   .Entity<ObjectModel>()
-      //   .HasData(
-      //     new ObjectModel(){
-      //       code = "1",
-      //       description = "Sapphire Objects",
-      //       docPrefix = null,
-      //       defaultSerieId = null
-      //     }
-      //     ,
-      //     new ObjectModel(){
-      //       code = "1.1",
-      //       description = "Series",
-      //       docPrefix = null,
-      //       defaultSerieId = null
-      //     }
-      //   );  
+      builder
+        .Entity<ObjectModel>()
+        .HasData(
+          new ObjectModel(){
+            code = "1",
+            description = "Sapphire Objects",
+            docPrefix = null,
+            defaultSerieId = null
+          }
+          ,
+          new ObjectModel(){
+            code = "1.1",
+            description = "Series",
+            docPrefix = null,
+            defaultSerieId = null
+          }
+        );  
     }
   }
 }
